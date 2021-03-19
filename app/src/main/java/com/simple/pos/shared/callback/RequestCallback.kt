8f1cad3846ea45179f6f78b7314a73490cb965b0
AanMFA <aanmfa06@gmail.com>
2021-03-19 14:11:31 +0700

    Fix Build Error #82

--- app/src/main/java/com/simple/pos/shared/callback/RequestCallback.kt
+++ app/src/main/java/com/simple/pos/shared/callback/RequestCallback.kt
@@ -1,9 +1,6 @@
 package com.simple.pos.shared.callback
 
-import com.simple.pos.modul.dailycashflow.model.Cashflow
-
 interface RequestCallback<T> {
     fun requestSuccess(data: T)
     fun requestError(message: String?)
-    fun requestSuccess(data: Array<Cashflow>)
 }
