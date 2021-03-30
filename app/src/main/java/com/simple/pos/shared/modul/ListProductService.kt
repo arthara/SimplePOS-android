bf9376f08f351d6365c8f83ce119e58ce0295133
AanMFA <aanmfa06@gmail.com>
2021-03-30 10:48:37 +0700

    Tampilkan foto produk di Belanja Dashboard #94

--- app/src/main/java/com/simple/pos/shared/modul/ListProductService.kt
+++ app/src/main/java/com/simple/pos/shared/modul/ListProductService.kt
@@ -1,10 +1,11 @@
 package com.simple.pos.shared.modul
 
+import com.simple.pos.shared.apiresponse.APIResponseCollection
 import com.simple.pos.shared.model.Product
 import retrofit2.http.GET
 import retrofit2.Call
 
 interface ListProductService {
     @GET("products")
-    fun retrieveProducts(): Call<Array<Product>?>
+    fun retrieveProducts(): Call<APIResponseCollection<Array<Product>?>>
 }
